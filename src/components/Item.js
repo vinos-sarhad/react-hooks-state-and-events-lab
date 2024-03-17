677e919a031116fdca84564237a6076e4a2429c4
Vinos Sarhad <vinos.sarhadd@gmail.com>
2024-03-17 23:53:09 +0300

    donee

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React ,{useState} from "react";
 
 function Item({ name, category }) {
+
+const[inCart,setIncart]=useState(false)
+const incartClass=inCart ? "in-cart" : ""
+function MarkInCart( ){
+setIncart(!inCart)
+
+if(inCart===true){
+  return{}
+}
+}
+
   return (
-    <li className="">
+    <li className={incartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={MarkInCart}>{inCart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
